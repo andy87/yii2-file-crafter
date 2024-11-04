7b59c2adea10987fe5ca9565b90c57b1f61192be
Andrey and _y87 Kidin <kidin_a@list.ru>
2024-11-04 12:42:09 +0300

    generator TR

--- src/web/js/file-crafter.js
+++ src/web/js/file-crafter.js
@@ -1,48 +1,139 @@
 let app = {};
 
 document.addEventListener('DOMContentLoaded', function () {
-    app =
-        {
-            data : {
-                target : {
+    app = {
+        dbFields : {
+            targets : {
 
-                    table_db_field : "#table_db_field",
-                    template_db_field : "#template_db_field",
+                table_db_field : "#table_db_field",
+                template_db_field : "#template_db_field",
+            },
+
+            getCountTR: function ()
+            {
+                return document.querySelectorAll(this.targets.table_db_field + ' tr').length;
+            },
+
+            getTemplateTr: function ()
+            {
+                return document.querySelector(this.targets.template_db_field).innerHTML;
+            },
+
+            changeIndex: function (HTML, index)
+            {
+                return HTML.replace(/\[0\]/g, `[${index}]`);
+            },
+
+            autocomplete : function(TR, fieldName)
+            {
+                //Some autocomplete
+                let option = null,
+                    size = null,
+                    checkBox = {
+                        isForeignKey : null,
+                        isUnique : null
+                    };
+
+
+                if ( fieldName.indexOf('_id') !== -1 )
+                {
+                    option = 'int';
+                    checkBox.isForeignKey = true;
+                    size = 8;
+                }
+
+                if ( fieldName.indexOf('mail') !== -1 )
+                {
+                    option = 'string';
+                    size = 64;
+                    checkBox.isUnique = true;
+                }
+
+                if ( fieldName.indexOf('date') !== -1 ) option = 'date';
+                if ( fieldName.indexOf('datetime') !== -1 ) option = 'datetime';
+                if ( fieldName.indexOf('timestamp') !== -1 ) option = 'timestamp';
+                if ( fieldName.indexOf('content') !== -1 ) option = 'text';
+                if ( fieldName.indexOf('html') !== -1 ) option = 'text';
+                if ( fieldName.indexOf('text') !== -1 ) option = 'text';
+                if ( fieldName.indexOf('name') !== -1 ) option = 'string';
+                if ( fieldName.indexOf('title') !== -1 ) option = 'string';
+
+                if ( fieldName.indexOf('header') !== -1 ) {
+                    option = 'string';
+                    size = 80;
+                }
+                if ( fieldName.indexOf('title') !== -1 ) {
+                    option = 'string';
+                    size = 80;
+                }
+
+                if (option)
+                {
+                    TR.querySelector('[data-db-field="type"]')
+                        .querySelector('select')
+                        .querySelector(`[value="${option}"]`)
+                        .setAttribute('selected', 'selected');
+                }
+
+                if (size)
+                {
+                    TR.querySelector('[data-db-field="size"]')
+                        .querySelector('input')
+                        .setAttribute('value', size);
+                }
+
+                if (checkBox.isForeignKey)
+                {
+                    TR.querySelector('[data-db-field="foreignKeys"]')
+                        .querySelector('input')
+                        .setAttribute('checked', 'checked');
+                }
+
+                if (checkBox.isUnique)
+                {
+                    TR.querySelector('[data-db-field="unique"]')
+                        .querySelector('input')
+                        .setAttribute('checked', 'checked');
                 }
             },
-            actions :
-                {
-                    addField: function (event)
-                    {
-                        let fieldName = prompt('Field name');
 
-                        let HTML = document.querySelector(app.data.target.template_db_field).innerHTML;
+            appendNewTR: function (HTML, fieldName)
+            {
+                //Кастыль! T_T
+                let tbody = document.createElement('tbody');
+                tbody.innerHTML = HTML;
 
-                        let index = document.querySelectorAll(app.data.target.table_db_field + ' tr').length;
+                let TR = tbody.querySelector('TR').cloneNode(true);
 
-                        HTML = HTML.replace(/\[0\]/g, `[${ index + 1 }]` );
+                let inputFieldName = TR
+                    .querySelector('[data-db-field="name"]')
+                    .querySelector('input');
 
-                        console.log('innerHTML', document.querySelector(app.data.target.table_db_field).innerHTML);
+                inputFieldName.value = fieldName;
 
-                        document.querySelector(app.data.target.table_db_field).innerHTML += HTML;
+                inputFieldName.setAttribute('value', fieldName);
 
-                        let inputFieldName = document
-                            .querySelector(app.data.target.table_db_field)
-                            .querySelector('TR:last-child')
-                            .querySelector('[data-db-field="name"]')
-                            .querySelector('input');
+                this.autocomplete(TR, fieldName);
 
-                        inputFieldName.value = fieldName;
+                document.querySelector(this.targets.table_db_field).append(TR);
+            },
+            addField: function ()
+            {
+                let fieldName = prompt('Field name'),
+                    HTML = this.getTemplateTr(),
+                    countTr = this.getCountTR() + 1;
 
-                        inputFieldName.setAttribute('value', fieldName);
-                    },
+                HTML = this.changeIndex(HTML, countTr);
 
-                    removeField(event) {
-                        //target
-                        let target = event.target();
+                this.appendNewTR(HTML, fieldName);
+            },
 
-                        console.log('event', event);
-                    }
-                }
-        };
+            removeField(event) {
+                //target
+                let target = event.target();
+
+                console.log('event', event);
+            }
+        },
+    };
 });
