dd395aeedbc13a9c6bfca3e644b9e9646d5469b3
Andrey and _y87 Kidin <kidin_a@list.ru>
2024-11-20 01:25:03 +0300

    tests functional

--- guide/examples/template/backend/tests/functional/items/ItemControllerCest.php
+++ guide/examples/template/backend/tests/functional/items/ItemControllerCest.php
@@ -1,146 +1,99 @@
 <?php declare(strict_types=1);
 
+namespace app\backend\tests\functional\items;
+
+use app\backend\tests\FunctionalTester;
 
 /**
- * < Backend > Тесты контроллера `ItemController`
+ * < Backend > Тесты контроллера `PascalCaseController`
  *
- * @package app\frontend\tests\functional
+ * @property  FunctionalTester $I
  *
- * @property \app\backend\tests\functional\controllers\items\FunctionalTester $I
- * @property \app\backend\tests\functional\controllers\items\ContactForm $form
+ * @cli ./vendor/bin/codecept run app/backend/tests/functional/PascalCaseControllerCest
  *
- * Fix not used:
- * - @see ContactCest::checkContact()
- * - @see ContactCest::checkContactSubmitNoData()
- * - @see ContactCest::checkContactSubmitNotCorrectEmail()
- * - @see ContactCest::checkContactSubmitCorrectData()
- *
- * @cli ./vendor/bin/codecept run app/backend/tests/functional/controllers/items/PascalCaseControllerCest
+ * @package app\backend\tests\functional\items
  *
  * @tag #backend #tests #functional #ContactCest
  */
 class PascalCaseControllerCest
 {
     /**
-     * @param \app\backend\tests\functional\controllers\items\FunctionalTester $I
+     * Тестирование экшена `index` контроллера `PascalCaseController`
+     *
+     * @cli ./vendor/bin/codecept run app/backend/tests/functional/PascalCaseControllerCest:checkIndex
+     *
+     * @param  FunctionalTester $I
      *
      * @return void
      *
-     * @see SiteController::actionContact()
-     *
-     * @tag #frontend #tests #functional #ContactCest #checkContact
+     * @tag #backend #tests #functional #action #index
      */
-    public function _before(\app\backend\tests\functional\controllers\items\FunctionalTester $I): void
+    public function checkIndex( FunctionalTester $I ): void
     {
-        parent::_before($I);
-
-        $route = \app\backend\tests\functional\controllers\items\SiteController::ENDPOINT . '/' . \app\backend\tests\functional\controllers\items\SiteController::ACTION_CONTACT;
-
-        $I->amOnRoute($route);
+        //$I->see( $this->form::TITLE, 'h1');
     }
 
     /**
-     * Check contact
+     * Тестирование экшена `create` контроллера `PascalCaseController`
      *
-     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/ContactCest:checkContact
+     * @cli ./vendor/bin/codecept run app/backend/tests/functional/PascalCaseControllerCest:checkCreate
      *
-     * @refer https://github.com/yiisoft/yii2-app-advanced/blob/master/frontend/tests/functional/ContactCest.php#L16
-     *
-     * @param \app\backend\tests\functional\controllers\items\FunctionalTester $I
+     * @param  FunctionalTester $I
      *
      * @return void
      *
-     * @tag #frontend #tests #functional #ContactCest #checkContact
+     * @tag #backend #tests #functional #action #create
      */
-    public function checkContact(\app\backend\tests\functional\controllers\items\FunctionalTester $I): void
+    public function checkCreate( FunctionalTester $I ): void
     {
-        $I->see( $this->form::TITLE, 'h1');
+        //$I->see($this->form::TITLE, 'h1');
     }
 
     /**
-     * Check contact submit no data
+     * Тестирование экшена `update` контроллера `PascalCaseController`
      *
-     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/ContactCest:checkContactSubmitNoData
+     * @cli ./vendor/bin/codecept run app/backend/tests/functional/PascalCaseControllerCest:checkUpdate
      *
-     * @refer https://github.com/yiisoft/yii2-app-advanced/blob/master/frontend/tests/functional/ContactCest.php#L21
-     *
-     * @param \app\backend\tests\functional\controllers\items\FunctionalTester $I
+     * @param  FunctionalTester $I
      *
      * @return void
      *
-     * @tag #frontend #tests #functional #ContactCest #checkContactSubmitNoData
+     * @tag #backend #tests #functional #action #update
      */
-    public function checkContactSubmitNoData(\app\backend\tests\functional\controllers\items\FunctionalTester $I): void
+    public function checkUpdate( FunctionalTester $I ): void
     {
-        $I->submitForm($this->formId, []);
-        $I->see($this->form::TITLE, 'h1');
-        $messages = [
-            str_replace('{attribute}', $this->form->getAttributeLabel($this->form::ATTR_NAME), $this->form::RULE_REQUIRED_MESSAGE),
-            str_replace('{attribute}', $this->form->getAttributeLabel($this->form::ATTR_EMAIL), $this->form::RULE_REQUIRED_MESSAGE),
-            str_replace('{attribute}', $this->form->getAttributeLabel($this->form::ATTR_SUBJECT), $this->form::RULE_REQUIRED_MESSAGE),
-            str_replace('{attribute}', $this->form->getAttributeLabel($this->form::ATTR_BODY), $this->form::RULE_REQUIRED_MESSAGE),
-        ];
-        foreach ($messages as $message) {
-            $I->seeValidationError($message);
-        }
-
-        $I->seeValidationError($this->form::RULE_VERIFY_CODE_MESSAGE);
+        //$I->see($this->form::TITLE, 'h1');
     }
 
     /**
-     * Check contact submit not correct email
+     * Тестирование экшена `view` контроллера `PascalCaseController`
      *
-     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/ContactCest:checkContactSubmitNotCorrectEmail
+     * @cli ./vendor/bin/codecept run app/backend/tests/functional/PascalCaseControllerCest:checkView
      *
-     * @refer https://github.com/yiisoft/yii2-app-advanced/blob/master/frontend/tests/functional/ContactCest.php#L32
-     *
-     * @param \app\backend\tests\functional\controllers\items\FunctionalTester $I
+     * @param  FunctionalTester $I
      *
      * @return void
      *
-     * @tag #frontend #tests #functional #ContactCest #checkContactSubmitNotCorrectEmail
+     * @tag #backend #tests #functional #action #view
      */
-    public function checkContactSubmitNotCorrectEmail(\app\backend\tests\functional\controllers\items\FunctionalTester $I): void
+    public function checkView( FunctionalTester $I ): void
     {
-        $I->submitForm($this->formId, [
-            "$this->formName[". \app\backend\tests\functional\controllers\items\ContactForm::ATTR_NAME."]" => 'tester',
-            "$this->formName[". \app\backend\tests\functional\controllers\items\ContactForm::ATTR_EMAIL."]" => 'tester.email',
-            "$this->formName[". \app\backend\tests\functional\controllers\items\ContactForm::ATTR_SUBJECT."]" => 'test subject',
-            "$this->formName[". \app\backend\tests\functional\controllers\items\ContactForm::ATTR_BODY."]" => 'test content',
-            "$this->formName[". \app\backend\tests\functional\controllers\items\ContactForm::ATTR_VERIFY_CODE."]" => \app\backend\tests\functional\controllers\items\CaptchaAction::TEST_VALUE,
-        ]);
-        $I->seeValidationError('Email is not a valid email address.');
-        $I->dontSeeValidationError('Name cannot be blank');
-        $I->dontSeeValidationError('Subject cannot be blank');
-        $I->dontSeeValidationError('Body cannot be blank');
-        $I->dontSeeValidationError('The verification code is incorrect');
+        //$I->see($this->form::TITLE, 'h1');
     }
 
     /**
-     * Check contact submit correct data
+     * Тестирование экшена `delete` контроллера `PascalCaseController`
      *
-     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/ContactCest:checkContactSubmitCorrectData
+     * @cli ./vendor/bin/codecept run app/backend/tests/functional/PascalCaseControllerCest:checkDelete
      *
-     * @refer https://github.com/yiisoft/yii2-app-advanced/blob/master/frontend/tests/functional/ContactCest.php#L48
-     *
-     * @param \app\backend\tests\functional\controllers\items\FunctionalTester $I
+     * @param  FunctionalTester $I
      *
      * @return void
      *
-     * @throws \app\backend\tests\functional\controllers\items\ModuleException
-     *
-     * @tag #frontend #tests #functional #ContactCest #checkContactSubmitCorrectData
+     * @tag #backend #tests #functional #action #delete
      */
-    public function checkContactSubmitCorrectData(\app\backend\tests\functional\controllers\items\FunctionalTester $I): void
+    public function checkDelete( FunctionalTester $I ): void
     {
-        $I->submitForm($this->formId, [
-            "$this->formName[".$this->form::ATTR_NAME."]" => 'tester',
-            "$this->formName[".$this->form::ATTR_EMAIL."]" => 'tester@example.com',
-            "$this->formName[".$this->form::ATTR_SUBJECT."]" => 'test subject',
-            "$this->formName[".$this->form::ATTR_BODY."]" => 'test content',
-            "$this->formName[".$this->form::ATTR_VERIFY_CODE."]" => \app\backend\tests\functional\controllers\items\CaptchaAction::TEST_VALUE,
-        ]);
-        $I->seeEmailIsSent();
-        $I->see($this->form::MESSAGE_SUCCESS);
+        //$I->see($this->form::TITLE, 'h1');
     }
 }
--- guide/examples/template/frontend/tests/functional/items/ItemControllerCest.php
+++ guide/examples/template/frontend/tests/functional/items/ItemControllerCest.php
@@ -1,1 +1,99 @@
 <?php declare(strict_types=1);
+
+namespace app\frontend\tests\functional\items;
+
+use app\frontend\tests\FunctionalTester;
+
+/**
+ * < Frontend > Тесты контроллера `PascalCaseController`
+ *
+ * @property  FunctionalTester $I
+ *
+ * @cli ./vendor/bin/codecept run app/frontend/tests/functional/PascalCaseControllerCest
+ *
+ * @package app\frontend\tests\functional\items
+ *
+ * @tag #frontend #tests #functional #ContactCest
+ */
+class PascalCaseControllerCest
+{
+    /**
+     * Тестирование экшена `index` контроллера `PascalCaseController`
+     *
+     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/PascalCaseControllerCest:checkIndex
+     *
+     * @param  FunctionalTester $I
+     *
+     * @return void
+     *
+     * @tag #frontend #tests #functional #action #index
+     */
+    public function checkIndex( FunctionalTester $I ): void
+    {
+        //$I->see( $this->form::TITLE, 'h1');
+    }
+
+    /**
+     * Тестирование экшена `create` контроллера `PascalCaseController`
+     *
+     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/PascalCaseControllerCest:checkCreate
+     *
+     * @param  FunctionalTester $I
+     *
+     * @return void
+     *
+     * @tag #frontend #tests #functional #action #create
+     */
+    public function checkCreate( FunctionalTester $I ): void
+    {
+        //$I->see($this->form::TITLE, 'h1');
+    }
+
+    /**
+     * Тестирование экшена `update` контроллера `PascalCaseController`
+     *
+     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/PascalCaseControllerCest:checkUpdate
+     *
+     * @param  FunctionalTester $I
+     *
+     * @return void
+     *
+     * @tag #frontend #tests #functional #action #update
+     */
+    public function checkUpdate( FunctionalTester $I ): void
+    {
+        //$I->see($this->form::TITLE, 'h1');
+    }
+
+    /**
+     * Тестирование экшена `view` контроллера `PascalCaseController`
+     *
+     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/PascalCaseControllerCest:checkView
+     *
+     * @param  FunctionalTester $I
+     *
+     * @return void
+     *
+     * @tag #frontend #tests #functional #action #view
+     */
+    public function checkView( FunctionalTester $I ): void
+    {
+        //$I->see($this->form::TITLE, 'h1');
+    }
+
+    /**
+     * Тестирование экшена `delete` контроллера `PascalCaseController`
+     *
+     * @cli ./vendor/bin/codecept run app/frontend/tests/functional/PascalCaseControllerCest:checkDelete
+     *
+     * @param  FunctionalTester $I
+     *
+     * @return void
+     *
+     * @tag #frontend #tests #functional #action #delete
+     */
+    public function checkDelete( FunctionalTester $I ): void
+    {
+        //$I->see($this->form::TITLE, 'h1');
+    }
+}
